280821969b0e76e00bfd962361c142ab82b795ea
kbdbvgm <kbd.bvgm@gmail.com>
2020-04-27 10:03:31 +0300

    Node init + install + webpack config.

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/declaration.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/,
+                use:
+                    [
+                        {
+                            loader: "file-loader",
+                            options: {
+                                name: '[name].css'
+                            }
+                        },
+                        'extract-loader',
+                        'css-loader',
+                        'resolve-url-loader',
+                        {
+                            loader: "sass-loader",
+                            options: {
+                                sourceMap: true,
+                            }
+                        }
+                    ],
+            },
+            {
+                test: /\.pug$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: '[name].html'
+                        }
+                    },
+                    'extract-loader',
+                    'html-loader',
+                    'pug-html-loader',
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: '[name].[ext]'
+                        }
+                    }
+                ],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: '[name].[ext]'
+                        }
+                    }
+                ]
+            }
+        ],
+   },
+    plugins: [
+        new CleanWebpackPlugin(),
+    ]
+};
